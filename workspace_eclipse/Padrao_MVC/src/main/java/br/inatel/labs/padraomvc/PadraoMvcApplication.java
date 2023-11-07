75ad62d07aac66b08f9c79d913b919583b1c80e5
Isaquehg <isaqueh.goncalves@gmail.com>
2023-11-06 21:05:29 -0300

    rest apis

--- workspace_eclipse/Padrao_MVC/src/main/java/br/inatel/labs/padraomvc/PadraoMvcApplication.java
+++ workspace_eclipse/Padrao_MVC/src/main/java/br/inatel/labs/padraomvc/PadraoMvcApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * SpringBoot server's start point
+ * SpringBoot server's starting point
  * @author Isaque
  * @version 1.0
  * @since 30/10/23
